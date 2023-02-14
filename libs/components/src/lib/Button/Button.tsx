929506449af77576e95aa96f289efbd71ec81a06
Richard Huf <richardhuf84@gmail.com>
2023-02-14 20:25:16 +1100

    chore: huskie

--- libs/components/src/lib/Button/Button.tsx
+++ libs/components/src/lib/Button/Button.tsx
@@ -18,11 +18,18 @@ const StyledButton = styled.button<NBButtonProps>`
 `;
 
 export const NBButton = forwardRef<HTMLButtonElement, NBButtonProps>(
-  ({ children, kind = 'primary',
+  (
+    {
+     
+     
+     
+      children,
+      kind = 'primary',
 
-
-
-    ...props }, ref) => (
+      ...props
+    },
+    ref
+  ) => (
     <StyledButton kind={kind} {...props} ref={ref}>
       {children}
     </StyledButton>
